5596dbd423ed84635f442305f80f1ccba54bbea9
Oracle Public Cloud User <opc@jc01.sub06071106580.sandboxvcn03.oraclevcn.com>
2021-10-12 11:25:48 +0900

    backend companyの修正

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "company"
+    organization = "test-company"
     workspaces {
       name = "tf-ecs-fargate"
     }
