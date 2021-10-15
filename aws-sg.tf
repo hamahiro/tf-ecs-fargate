915151ac8f294bf40f14eb62e19585a2688edfb5
Oracle Public Cloud User <opc@jc01.sub06071106580.sandboxvcn03.oraclevcn.com>
2021-10-15 17:50:45 +0900

    Security Groupリソース名の変更

--- aws-sg.tf
+++ aws-sg.tf
@@ -1,24 +1,24 @@
 ## aws-sg.tf
 # Security Group
-resource "aws_security_group" "sg_alb" {
+resource "aws_security_group" "alb" {
   name        = "sg-alb-${terraform.workspace}"
   description = "for ALB"
   vpc_id      = aws_vpc.vpc.id
 }
  
-resource "aws_security_group" "sg_fargate" {
+resource "aws_security_group" "fargate" {
   name        = "sg-fargate-${terraform.workspace}"
   description = "for Fargate"
   vpc_id      = aws_vpc.vpc.id
 }
  
-resource "aws_security_group" "sg_efs" {
-  name        = "sg-efs--${terraform.workspace}"
+resource "aws_security_group" "efs" {
+  name        = "sg-efs-${terraform.workspace}"
   description = "for EFS"
   vpc_id      = aws_vpc.vpc.id
 }
  
-resource "aws_security_group" "sg_rds" {
+resource "aws_security_group" "rds" {
   name        = "sg-rds-${terraform.workspace}"
   description = "for RDS"
   vpc_id      = aws_vpc.vpc.id
@@ -26,7 +26,7 @@ resource "aws_security_group" "sg_rds" {
  
 # Security Group Rule
 resource "aws_security_group_rule" "allow_http_for_alb" {
-  security_group_id = aws_security_group.sg_alb.id
+  security_group_id = aws_security_group.alb.id
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 80
@@ -36,37 +36,37 @@ resource "aws_security_group_rule" "allow_http_for_alb" {
 }
  
 resource "aws_security_group_rule" "from_alb_to_fargate" {
-  security_group_id        = aws_security_group.sg_fargate.id
+  security_group_id        = aws_security_group.fargate.id
   type                     = "ingress"
   protocol                 = "tcp"
   from_port                = 80
   to_port                  = 80
-  source_security_group_id = aws_security_group.sg_alb.id
+  source_security_group_id = aws_security_group.alb.id
   description              = "from ALB to Fargate"
 }
  
 resource "aws_security_group_rule" "from_fargate_to_efs" {
-  security_group_id        = aws_security_group.sg_efs.id
+  security_group_id        = aws_security_group.efs.id
   type                     = "ingress"
   protocol                 = "tcp"
   from_port                = 2049
   to_port                  = 2049
-  source_security_group_id = aws_security_group.sg_fargate.id
+  source_security_group_id = aws_security_group.fargate.id
   description              = "from Fargate to EFS"
 }
  
 resource "aws_security_group_rule" "from_fargate_to_rds" {
-  security_group_id        = aws_security_group.sg_rds.id
+  security_group_id        = aws_security_group.rds.id
   type                     = "ingress"
   protocol                 = "tcp"
   from_port                = 3306
   to_port                  = 3306
-  source_security_group_id = aws_security_group.sg_fargate.id
+  source_security_group_id = aws_security_group.fargate.id
   description              = "from Fargate to RDS"
 }
 
 resource "aws_security_group_rule" "eggress_alb" {
-  security_group_id = aws_security_group.sg_alb.id
+  security_group_id = aws_security_group.alb.id
   type              = "egress"
   protocol          = "-1"
   from_port         = 0
@@ -76,7 +76,7 @@ resource "aws_security_group_rule" "eggress_alb" {
 }
  
 resource "aws_security_group_rule" "eggress_fargate" {
-  security_group_id = aws_security_group.sg_fargate.id
+  security_group_id = aws_security_group.fargate.id
   type              = "egress"
   protocol          = "-1"
   from_port         = 0
@@ -86,7 +86,7 @@ resource "aws_security_group_rule" "eggress_fargate" {
 }
  
 resource "aws_security_group_rule" "eggress_efs" {
-  security_group_id = aws_security_group.sg_efs.id
+  security_group_id = aws_security_group.efs.id
   type              = "egress"
   protocol          = "-1"
   from_port         = 0
@@ -96,7 +96,7 @@ resource "aws_security_group_rule" "eggress_efs" {
 }
  
 resource "aws_security_group_rule" "eggress_rds" {
-  security_group_id = aws_security_group.sg_rds.id
+  security_group_id = aws_security_group.rds.id
   type              = "egress"
   protocol          = "-1"
   from_port         = 0
