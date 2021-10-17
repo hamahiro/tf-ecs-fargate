43858d8ce2898af7431530b0cc905108ed801356
Oracle Public Cloud User <opc@jc01.sub06071106580.sandboxvcn03.oraclevcn.com>
2021-10-17 19:48:33 +0900

    RDSの暗号化設定

--- aws-rds.tf
+++ aws-rds.tf
@@ -24,6 +24,7 @@ resource "aws_db_instance" "rds" {
   storage_type              = "gp2"
   allocated_storage         = var.rds.dbgsize
   max_allocated_storage     = "3000"
+  storage_encrypted         = "true"
   username                  = var.rds.username
   password                  = var.rds.password
   final_snapshot_identifier = "rds-final-${terraform.workspace}"
