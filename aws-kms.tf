4d94f5a0e4ae5aec1dbd8ddc5e56f6612c069387
Oracle Public Cloud User <opc@jc01.sub06071106580.sandboxvcn03.oraclevcn.com>
2021-10-18 17:37:42 +0900

    KMSリソース定義時の誤字修正

--- aws-kms.tf
+++ aws-kms.tf
@@ -3,11 +3,11 @@
 resource "aws_kms_key" "kms_key" {
   description             = "CMK for ${terraform.workspace}"
   enable_key_rotation     = true
-  is_enables              = true
+  is_enabled              = true
   deletion_window_in_days = 30
 }
 
 resource "aws_kms_alias" "kms_alias" {
   name          = "alias/${terraform.workspace}_key"
-  target_key_id = aws_kems_key.kms_key.key_id
+  target_key_id = aws_kms_key.kms_key.key_id
 }
