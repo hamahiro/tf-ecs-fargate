daafa160842e5147c7d27c2d4b618057f34e7970
Oracle Public Cloud User <opc@jc01.sub06071106580.sandboxvcn03.oraclevcn.com>
2021-10-22 21:37:43 +0900

    Fargete用のIAMロールの作成

---
+++ aws-iam.tf
@@ -0,0 +1,12 @@
+## aws-iam.tf
+# iam role
+resource "aws_iam_role" "fargate_task_execution" {
+  name               = "role-fargate_task_execution-${terraform.workspace}"
+  assume_role_policy = file(./roles/fargate_task_assume_role.json)
+}
+
+# attach AmazonECSTaskExecutionRolePolicy
+resource "aws_iam_policy_attachment" "attach" {
+  role       = aws_iam_role.fargate_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
